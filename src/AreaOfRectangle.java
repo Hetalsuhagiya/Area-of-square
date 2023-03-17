76131abe37be4de7c4db6120ac0ccecd1f7d2415
hetal <suhagiyahetal23@gmail.com>
2023-03-17 21:25:41 +0000

    second commit

---
+++ src/AreaOfRectangle.java
@@ -0,0 +1,10 @@
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        int length= 9;
+        int width=5;
+        int area=length*width;
+        System.out.println("Area of the rectangle="+area);
+
+    }
+
+}
